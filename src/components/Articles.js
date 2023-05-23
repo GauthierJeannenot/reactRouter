650cbfbe1a39310e774a53b64df9a17fc502bd56
Jeannenot Gauthier <jeannenot.gauthier@gmail.com>
2023-05-23 10:56:22 +0200

    first commit

---
+++ src/components/Articles.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link, useParams } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
+import Search from "./Search";
+
+// Import Link and useSearchParams from React Router
+
+export default function Articles () {
+  const articles = useSelector(selectArticles);
+
+  // Grab URLSearchParams object from useSearchParams hook
+  
+
+  
+  // Get the queryParams from object returned from useSearchParams and set to `title`
+  const title = '';
+
+  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
+
+  return (
+    <main>
+      <h1>Articles</h1>
+      <ul>
+        { filteredArticles.map(article => (
+          <li key={article.slug}>
+            {/* Replace these a tags! */}
+            <Link to={`${article.slug}`}>
+              {article.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Search />
+    </main>
+  )
+}
